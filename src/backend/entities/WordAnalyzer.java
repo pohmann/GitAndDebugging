643b686c38811e06917f9df0d6322d5ab0abee58
Peter Ohmann <ohmann@cs.wisc.edu>
2024-01-29 16:28:03 -0600

    Add WordAnalyzer class
    - intended to help us filter junk names from the DB
    - but...it has bugs!?

---
+++ src/backend/entities/WordAnalyzer.java
@@ -0,0 +1,76 @@
+package backend.entities;
+
+/** 
+ * A class that analyzes words.
+ */
+public class WordAnalyzer{
+    
+  private String word;
+  
+  /**
+   * Constructs an analyzer for a given word.
+   * @param aWord the word to be analyzed 
+   */
+  public WordAnalyzer(String aWord){
+    word = aWord;
+  }
+  
+  /**
+   * Gets the first repeated character. A character is repeated
+   * if it occurs at least twice in adjacent positions. For example,
+   * 'l' is repeated in "hollow", but 'o' is not. 
+   * @return the first repeated character, or 0 if none found
+   */
+  public char firstRepeatedCharacter(){
+    for (int i = 0; i < word.length(); i++){
+      char ch = word.charAt(i);
+      if (ch == word.charAt(i + 1))
+        return ch;
+    }
+    return 0;
+  }
+  
+  /** 
+   * Gets the first multiply occuring character. A character is 
+   * "multiple" if it occurs at least twice in the word, not necessarily in    
+   * in adjacent positions. 
+   * For example, both 'o' and 'l' are multiple in "hollow", but 'h' is   
+   * not. 
+   * @return the first repeated character, or 0 if none found 
+   */ 
+  
+  public char firstMultipleCharacter(){
+    for (int i = 0; i < word.length(); i++){
+      char ch = word.charAt(i);
+      if (find(ch, i+1) >= 0)
+        return ch;
+    }
+    return 0;
+  }
+  
+  private int find(char c, int pos){
+    for (int i = pos; i < word.length(); i++){
+      if (word.charAt(i) == c) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  /**
+   * Counts the groups of repeated characters. For example, 
+   * "mississippi!!!" has 
+   * four such groups: ss, ss, pp and !!!.
+   * @return the number of repeated character groups 
+   */
+  public int countRepeatedCharacters(){
+    int c = 0;
+    for (int i = 1; i < word.length() - 1; i++){
+      if (word.charAt(i) == word.charAt(i + 1)){ // found a repetition
+        if (word.charAt(i - 1) != word.charAt(i)) // it's the start
+          c++;
+      }
+    }     
+    return c;
+  }
+}
--- src/frontend/Driver.java
+++ src/frontend/Driver.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 import backend.database.DatabaseController;
 import backend.entities.User;
+import backend.entities.WordAnalyzer;
 
 /**
  * The Driver class is used to demonstrate a bit of database functionality.
@@ -15,19 +16,20 @@ import backend.entities.User;
  */
 public class Driver {
 
-	public static void main(String[] args) {
+	// PART 1 ONLY!
+	private static void part1() {
 		@SuppressWarnings("resource")
 		Scanner s = new Scanner(System.in);
-		
+
 		// ask the user for a username to add to the DB (for testing)
 		System.out.print("What username should I add to the database (must be unique!)?: ");
 		String username = s.nextLine();
 		System.out.print("What password should I assign to that user?: ");
 		String password = s.nextLine();
-		
+
 		// add the user to the database
 		DatabaseController.addUser(username, password);
-		
+
 		// get the list of all users in the database,
 		// and print out the  to prove that it added
 		// successfully!
@@ -35,10 +37,49 @@ public class Driver {
 		for (User u : allUsers) {
 			System.out.println(u.getUsername() + " | " + u.getPassword());
 		}
-		
+
 		// remove the user we added from the database, so we don't leave
 		// junk test data in the database for future users
 		DatabaseController.removeUser(username);
+	}
+
+	// PART 2 ONLY!
+	private static void testWAFirst(String s) {
+		WordAnalyzer wa = new WordAnalyzer(s);
+		char result = wa.firstRepeatedCharacter();
+		if (result == 0)
+			System.out.println("No repeated character.");
+		else
+			System.out.println("First repeated character = " + result);
+	}
+
+	// PART 2 ONLY!
+	private static void testWACount(String s)
+	{
+		WordAnalyzer wa = new WordAnalyzer(s);
+		int result = wa.countRepeatedCharacters();
+		System.out.println(result + " repeated characters.");
+	}
+
+	// PART 2 ONLY!
+	// a test for the WordAnalyzer...not currently set to run
+	// because of bugs...
+	// We should write some regression tests and fix the bugs!
+	private static void part2() {
+		testWAFirst("aardvark"); // expect: a
+		testWAFirst("roommate"); // expect: o (not m)
+		testWAFirst("mate"); // expect: 0 (no duplicate letters)
+		testWAFirst("test"); // expect: 0 (the t isn't repeating)
+
+		testWACount("mississippiii"); // expect: 4 (ss, ss, pp, iii)
+		testWACount("test"); // expect: 0 (no repeated letters)
+		testWACount("aabbcdaaaabb"); // expect: 4 (aa, bb, aaaa, bb)
+	}
+
+	// This is main.  It calls the other methods to run the "program"!
+	public static void main(String[] args) {
+		part1();
+		//part2();
 	}
 
 }
